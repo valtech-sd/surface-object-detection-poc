fec4e52fa4760e961653c156c18f0c3f2b61c9a0
Gonzalo Zunino <gonzalo.zunino@gmail.com>
2022-08-04 22:52:29 -0300

    Filtering detections by cellphone class

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,4 +1,5 @@
 import { DetectedObject } from "@tensorflow-models/coco-ssd";
+import { ModelDetectionClasses } from "./modelDetectionClasses";
 
 export const drawDetections = (
   detections: DetectedObject[],
@@ -9,15 +10,18 @@ export const drawDetections = (
       const [x, y, width, height] = detection.bbox;
       const text = detection.class;
 
-      const color = "black";
-      canvasContext.strokeStyle = color;
-      canvasContext.font = "18px Arial";
-      canvasContext.fillStyle = color;
+      if (text === ModelDetectionClasses.CELL_PHONE) {
+        const color = "black";
 
-      canvasContext.beginPath();
-      canvasContext.fillText(text, x, y);
-      canvasContext.rect(x, y, width, height);
-      canvasContext.stroke();
+        canvasContext.strokeStyle = color;
+        canvasContext.font = "18px Arial";
+        canvasContext.fillStyle = color;
+
+        canvasContext.beginPath();
+        canvasContext.fillText(text, x, y);
+        canvasContext.rect(x, y, width, height);
+        canvasContext.stroke();
+      }
     });
   }
 };
