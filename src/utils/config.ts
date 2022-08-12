fb1caabc2d1c72f3a46121465849a9b7362c3786
Eric Lifs <lifszyc.eric@gmail.com>
2022-08-12 17:41:58 -0300

    feat: add usage of env file variables

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,7 +7,7 @@ export const BALL_RADIUS = 10;
 export const BALL_COLOR = "white";
 export const BALL_SPEED = 5;
 export const COMPUTER_LEVEL = 0.1;
-export const GAME_ID = "nintendo";
+export const GAME_ID = process.env.REACT_APP_GAME_ID as string;
 
 export const BOARD_X_START = window.innerWidth * 0.1 + 10;
 export const BOARD_X_MIDDLE = window.innerWidth / 2;
